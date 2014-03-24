cda51dec546ad04e93943ca6d5240f74b2c90660
rob <rob@whiskey.(none)>
2014-03-24 07:57:37 -0400

    initial commit

---
+++ build.sbt
@@ -0,0 +1,25 @@
+name := "GDAL-scala"
+
+scalaVersion := "2.10.3"
+
+fork in run := true
+
+// resolvers ++= Seq(
+//
+// )
+
+libraryDependencies ++= Seq(
+  "com.github.scopt" % "scopt_2.10" % "3.2.0"
+)
+
+// assemblySettings
+
+// mergeStrategy in assembly <<= (mergeStrategy in assembly) {
+//   (old) => {
+//     case "reference.conf" => MergeStrategy.concat
+//     case "application.conf" => MergeStrategy.concat
+//     case "META-INF/MANIFEST.MF" => MergeStrategy.discard
+//     case "META-INF\\MANIFEST.MF" => MergeStrategy.discard
+//     case _ => MergeStrategy.first
+//   }
+// }
---
+++ src/main/scala/gdal/Main.scala
@@ -0,0 +1,43 @@
+package gdal
+
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.Vector;
+
+import org.gdal.gdal.Band
+import org.gdal.gdal.ColorTable
+import org.gdal.gdal.Dataset
+import org.gdal.gdal.Driver
+import org.gdal.gdal.GCP
+import org.gdal.gdal.gdal
+import org.gdal.gdal.TermProgressCallback
+import org.gdal.gdal.RasterAttributeTable
+import org.gdal.gdalconst.gdalconstConstants
+import org.gdal.osr.CoordinateTransformation
+import org.gdal.osr.SpatialReference
+
+import org.gdal.ogr.Geometry
+
+//case class Options(
+
+
+object Main {
+  val usage = "Usage: gdalinfo [--help-general] [-mm] [-stats] [-hist] [-nogcp] [-nomd]\n               [-norat] [-noct] [-mdd domain]* [-checksum] datasetname"
+
+  def main(args:Array[String]): Unit = {
+    gdal.AllRegister()
+
+    val processedArgs = gdal.GeneralCmdLineProcessor(args)
+
+    if(processedArgs.isEmpty) {
+      println(usage)
+      return
+    }
+
+
+
+    val geom = Geometry.CreateFromWkt("POINT (1 2)")
+    val wkt = geom.ExportToWkt
+    println(wkt)
+  }
+}
