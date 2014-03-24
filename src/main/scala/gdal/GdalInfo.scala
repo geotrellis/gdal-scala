52fb68d7a1cf20911960e5945f36cb3816c00921
rob <rob@whiskey.(none)>
2014-03-24 08:57:57 -0400

    Scopt parser working.

--- src/main/scala/gdal/GdalInfo.scala
+++ src/main/scala/gdal/GdalInfo.scala
@@ -21,17 +21,16 @@ import org.gdal.ogr.Geometry
 object GdalInfo {
   val usage = "Usage: gdalinfo [--help-general] [-mm] [-stats] [-hist] [-nogcp] [-nomd]\n               [-norat] [-noct] [-mdd domain]* [-checksum] datasetname"
 
-  def main(args:Array[String]): Unit = {
-    gdal.AllRegister()
-
-    val processedArgs = gdal.GeneralCmdLineProcessor(args)
-
-    if(processedArgs.isEmpty) {
-      println(usage)
-      return
+  def main(args:Array[String]): Unit =
+    GdalInfoOptions.parse(args) match {
+      case Some(options) =>
+        apply(options)
+      case None =>
+        // Argument errored, should have printed usage.
     }
 
-
+  def apply(options: GdalInfoOptions): Unit = {
+    gdal.AllRegister()
 
     val geom = Geometry.CreateFromWkt("POINT (1 2)")
     val wkt = geom.ExportToWkt
