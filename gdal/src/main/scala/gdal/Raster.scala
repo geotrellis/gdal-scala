5446d118657bca6c5e7216fc18cbbeb1320250b9
Rob Emanuele <rdemanuele@gmail.com>
2014-03-25 07:21:32 -0400

    Added types and bands'

--- gdal/src/main/scala/gdal/Raster.scala
+++ gdal/src/main/scala/gdal/Raster.scala
@@ -1,6 +1,7 @@
 package gdal
 
 import org.gdal.gdal.Dataset
+import org.gdal.gdal.Band;
 import org.gdal.gdal.Driver
 import org.gdal.gdal.GCP
 import org.gdal.osr.SpatialReference
@@ -53,7 +54,56 @@ class Raster(val ds: Dataset) {
 
   def metadata(id: String): List[String] = 
     ds.GetMetadata_List(id).toList.map(_.asInstanceOf[String])
+
+  lazy val bands: Vector[RasterBand] = 
+    (1 to ds.getRasterCount)
+      .map { i => new RasterBand(ds.GetRasterBand(i)) }
+      .toVector
+
 }
+
+class RasterBand(band: Band) {
+  lazy val noDataValue: Double = {
+    val arr = Array.ofDim[java.lang.Double](1)
+    band.GetNoDataValue(arr)
+    arr(0)
+  }
+
+  lazy val rasterType: GdalDataType =
+    band.getDataType()
+}
+
+object GdalDataType {
+  implicit def intToGdalDataType(i: Int): GdalDataType =
+    i match {
+      case  0 => TypeUnknown
+      case  1 => TypeByte
+      case  2 => TypeUInt16
+      case  3 => TypeInt16
+      case  4 => TypeUInt32
+      case  5 => TypeInt32
+      case  6 => TypeFloat32
+      case  7 => TypeFloat64
+      case  8 => TypeCInt16
+      case  9 => TypeCInt32
+      case 10 => TypeCFloat32
+      case 11 => TypeCFloat64
+    }
+}
+
+abstract sealed class GdalDataType
+case object TypeUnknown extends GdalDataType
+case object TypeByte extends GdalDataType
+case object TypeUInt16 extends GdalDataType
+case object TypeInt16 extends GdalDataType
+case object TypeUInt32 extends GdalDataType
+case object TypeInt32 extends GdalDataType
+case object TypeFloat32 extends GdalDataType
+case object TypeFloat64 extends GdalDataType
+case object TypeCInt16 extends GdalDataType
+case object TypeCInt32 extends GdalDataType
+case object TypeCFloat32 extends GdalDataType
+case object TypeCFloat64 extends GdalDataType
 
 case class GroundControlPoint(id: String,
                               info: String,
--- geotrellis/src/main/scala/geotrellis/gdal/GdalReader.scala
+++ geotrellis/src/main/scala/geotrellis/gdal/GdalReader.scala
@@ -1,15 +1,37 @@
 package geotrellis.gdal
 
 import gdal.Gdal
+import _root_.{gdal => GDAL}
 import gdal.{Raster => GdalRaster}
 
 import geotrellis._
 
 object GdalReader {
-  def read(path: String): Raster = {
+  def read(path: String, band: Int = 1): Raster = {
     val gdalRaster: GdalRaster = Gdal.open(path)
 
-//    val extent = 
+    val extent = Extent(gdalRaster.xmin, 
+                        gdalRaster.ymin,
+                        gdalRaster.xmax,
+                        gdalRaster.ymax)
+    val rasterExtent = RasterExtent(extent, gdalRaster.cols, gdalRaster.rows)
+
+    val rasterBand = gdalRaster.bands(band)
+    val rasterType = rasterBand.rasterType match {
+      case TypeUnknown => geotrellis.TypeDouble
+      case TypeByte => geotrellis.TypeByte
+      case TypeUInt16 => geotrellis.TypeInt
+      case TypeInt16 => geotrellis.TypeShort
+      case TypeUInt32 => geotrellis.TypeFloat
+      case TypeInt32 => geotrellis.TypeInt
+      case TypeFloat32 => geotrellis.TypeFloat
+      case TypeFloat64 => geotrellis.TypeDouble
+      case TypeCInt16 => ???
+      case TypeCInt32 => ???
+      case TypeCFloat32 => ???
+      case TypeCFloat64 => ???
+    }
+
     ???
   }
 }
