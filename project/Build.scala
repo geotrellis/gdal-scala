87a62d7ec8c506739494b33a102d53f6c75caef7
lossyrob <rdemanuele@gmail.com>
2014-04-02 17:24:30 -0400

    Moved geotrellis stuff to GeoTrellis repo.

--- project/Build.scala
+++ project/Build.scala
@@ -16,69 +16,24 @@ object GdalBuild extends Build {
 
   lazy val gdal =
     Project("root", file("gdal")).settings(
-      organization := "org.gdal.gdal",
+      organization := "com.azavea.geotrellis",
       name := "gdal-scala",
       version := "0.1.0-SNAPSHOT",
       scalaVersion := "2.10.3",
      
       scalacOptions ++= scalaOptions,
-      fork in run := true,
-      fork in test := true,
+      fork := true,
       javaOptions += "-Djava.library.path=/usr/local/lib",
 
       mainClass := Some("gdal.Main"),
 
       libraryDependencies ++= Seq(
+        "org.gdal" % "gdal" % "1.9.2",
         "com.github.scopt" % "scopt_2.10" % "3.2.0"
-      )
-    )
-
-  lazy val geotrellis =
-    Project("geotrellis", file("geotrellis")).settings(
-      organization := "com.azavea.geotrellis",
-      name := "geotrellis-gdal",
-      version := "0.9.0",
-      scalaVersion := "2.10.3",
-
-      scalacOptions ++= scalaOptions,
-      fork in run := true,
-      fork in test := true,
-      javaOptions += "-Djava.library.path=/usr/local/lib",
-
-      libraryDependencies ++= Seq(
-        "com.azavea.geotrellis" %% "geotrellis" % "0.9.0",
-        "com.azavea.geotrellis" %% "geotrellis-geotools" % "0.9.0" % "test",
-        "org.scalatest"         %%  "scalatest"  % "2.0.M5b" % "test"
       ),
-      resolvers ++= 
+      resolvers ++=
         Seq(
-          "Geotools" at "http://download.osgeo.org/webdav/geotools/"
+          "OpenGeo" at "http://repo.opengeo.org/"
         )
-    ).dependsOn(gdal)
-
-  // Project: benchmark
-
-  val key = AttributeKey[Boolean]("javaOptionsPatched")
-  lazy val benchmark: Project =
-    Project("benchmark", file("benchmark"))
-      .settings(
-      organization := "org.gdal.gdal",
-        name := "gdal-benchmark",
-
-      scalaVersion := "2.10.3",
-        // raise memory limits here if necessary
-        javaOptions += "-Xmx2G",
-        javaOptions += "-Djava.library.path=/usr/local/lib",
-
-      libraryDependencies ++= Seq(
-        "com.azavea.geotrellis" %% "geotrellis-geotools" % "0.9.0"
-      ),
-        resolvers ++=
-          Seq(
-            "Geotools" at "http://download.osgeo.org/webdav/geotools/"
-          ),
-
-      // enable forking in both run and test
-      fork := true
-    ).dependsOn(geotrellis)
+    )
 }
